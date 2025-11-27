8ab52327ab72a3ba19208846f30df9c367a21a3b
Leogeox <leovginx@gmail.com>
2025-11-27 19:37:52 +0100

    feat(Leo): added subpages to front

--- caroleb-theme/page-galerie.php
+++ caroleb-theme/page-galerie.php
@@ -2,7 +2,37 @@
 
 get_header();
 
-the_title();
 ?>
+
+<main id="main-content" class="site-main">
+    <h1 class="h1"><?php the_title(); ?> </h1>
+
+    <nav class="galerie-nav">
+        <?php
+        $subpages = get_pages([
+            'child_of' => get_the_ID(),
+            'sort_column' => 'menu_order'
+        ]);
+
+        if ($subpages) {
+            echo '<ul class="nav-list">';
+            foreach ($subpages as $page) {
+                echo '<li class="list-individual">
+                <a class="list-a" href="' . get_permalink($page->ID) . '">' . $page->post_title . '</a>
+                </li>';
+            }
+            echo '</ul>';
+        }
+        ?>
+    </nav>
+
+    <section class="galerie-wrapper">
+    </section>
+
+    <!-- <nav class="container"></nav> -->
+</main><!-- #main-content -->
+
+
+
 <?php
 get_footer();
